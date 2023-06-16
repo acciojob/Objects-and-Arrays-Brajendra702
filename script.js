1388b800c49a7b3be6712ca9f341d31a3e2e20ef
Brajendra singh umath <66347784+Brajendra702@users.noreply.github.com>
2023-06-16 20:56:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-const team=players;//store the reference
-const team1=[...players];//copy of player
-const cap1={...players};//copy person object
+// Create a reference to the players array
+const team = players;
+
+// Create a copy of the players array
+const team1 = [...players];
+
+// Create a copy of the person object
+const cap1 = {...person};
+
 window.players = players;
 window.person = person;
 window.team = team;
